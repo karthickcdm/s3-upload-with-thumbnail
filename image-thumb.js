e543ab68b25189768768245641af040403bea397
karthickkumararaja <78716125+karthickkumararaja@users.noreply.github.com>
2021-11-26 17:30:01 +0530

    message changes

--- image-thumb.js
+++ image-thumb.js
@@ -144,4 +144,4 @@ app.post('/upload', upload.single('image'), (req, res, next) => {
     }
 });
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+app.listen(port, () => console.log(`Image Thumbnail Upload app listening at port: ${port}!`));
